1566437594186ba31674dbd3ce331fc11fe8ea45
BobbyButtins <122577730+BobbyButtins@users.noreply.github.com>
2023-06-07 19:33:57 -0700

    Delete battery.py

--- battery.py
+++
@@ -1,9 +0,0 @@
-# Battery Status
-# [no battery yet to test]
-
-status = "/sys/class/power_supply/BAT0/capacity"
-with open(status) as f:
-    while True:
-        power = f.read()
-        print(power)
-
